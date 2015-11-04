c6fad4f93c0899505de4fc7fb05c792fb0ebc0f5
Jean Yang <jean.yang.writeme@gmail.com>
2015-11-04 12:01:36 -0500

    renamed everything to use actual biology names

--- small_tests/python/simple_inferences.py
+++ small_tests/python/simple_inferences.py
@@ -4,49 +4,49 @@
 """
 from z3 import *
 
-# Sites.
-Site = DeclareSort('Site')
-MEK = Const('MEK', Site)
+# Proteins.
+Protein = DeclareSort('Protein')
+MEK = Const('MEK', Protein)
 
 # Activities.
-active = Function('active', Site, BoolSort())
-kinase_active = Function('kinase_active', Site, BoolSort())
+active = Function('active', Protein, BoolSort())
+kinase_active = Function('kinase_active', Protein, BoolSort())
 
-# More on kinases.
-KinaseFamily = DeclareSort('KinaseFamily')
-Serine = Const('Serine', KinaseFamily)
-KinaseMolecule = DeclareSort('KinaseMolecule')
-S222 = Const('S222', KinaseMolecule)
-KinaseLabel = Datatype('KinaseLabel')
-KinaseLabel.declare('none')
-KinaseLabel.declare('family', ('f', KinaseFamily))
-KinaseLabel.declare('kinase', ('k', KinaseMolecule))
-KinaseLabel = KinaseLabel.create()
+# More on proteins and sites.
+Residue = DeclareSort('Residue')
+Serine = Const('Serine', Residue)
+Site = DeclareSort('Site')
+S222 = Const('S222', Site)
+SiteLabel = Datatype('SiteLabel')
+SiteLabel.declare('none')
+SiteLabel.declare('residue', ('f', Residue))
+SiteLabel.declare('site', ('k', Site))
+SiteLabel = SiteLabel.create()
 
-kinase_subsumes = Function('kinase_subsumes'
-    , KinaseLabel, KinaseLabel, BoolSort())
-in_kinase_family = Function('in_kinase_family'
-    , KinaseLabel, KinaseLabel, BoolSort())
+site_refines = Function('site_refines'
+    , SiteLabel, SiteLabel, BoolSort())
+in_family = Function('in_family'
+    , SiteLabel, SiteLabel, BoolSort())
 phosphorylated = Function('phosphorylated'
-    , Site, KinaseLabel, BoolSort())
+    , Protein, SiteLabel, BoolSort())
 
-s = Const('s', Site)
-k1 = Const('k1', KinaseLabel)
-k2 = Const('k2', KinaseLabel)
+s = Const('s', Protein)
+k1 = Const('k1', SiteLabel)
+k2 = Const('k2', SiteLabel)
 inferenceRules = [
     ForAll(s, Implies(kinase_active(s), active(s)))
     # Subsumption relationship based on "family" relationship.
     , ForAll(k1
         , ForAll(k2
-            , Implies(in_kinase_family(k1, k2), kinase_subsumes(k2, k1))))
+            , Implies(in_family(k1, k2), site_refines(k2, k1))))
     # Having some information subsumes having no information.
-    , ForAll(k1, kinase_subsumes(k1, KinaseLabel.none))
+    , ForAll(k1, site_refines(k1, SiteLabel.none))
     # If we know something more specific phosphorylates a site, then we can
     # infer the more general phosphorylation.
     , ForAll(s
         ,   ForAll(k1
             , ForAll(k2
-                , Implies(And(kinase_subsumes(k2, k1), phosphorylated(s, k2))
+                , Implies(And(site_refines(k2, k1), phosphorylated(s, k2))
                     , phosphorylated(s, k1)))))
 ]
 
@@ -74,7 +74,7 @@ class SimpleInferenceEngine:
 
 def testActivity(engine):
     engine.push()
-    s1 = Const('s1', Site)
+    s1 = Const('s1', Protein)
     engine.z3_assert(kinase_active(s1))
 
     # Should be unsat.
@@ -82,22 +82,22 @@ def testActivity(engine):
     engine.pop()
 
 def testPhosphorylation(engine):
-    serine = KinaseLabel.family(Serine)
-    s222 = KinaseLabel.kinase(S222)
+    serine = SiteLabel.residue(Serine)
+    s222 = SiteLabel.site(S222)
 
     engine.push()
-    engine.z3_assert(in_kinase_family(serine, s222))
+    engine.z3_assert(in_family(serine, s222))
     engine.z3_assert(phosphorylated(MEK, s222))
-    engine.z3_assert(kinase_subsumes(s222, serine))
+    engine.z3_assert(site_refines(s222, serine))
 
     # Sanity checks.
-    engine.check(kinase_subsumes(s222, KinaseLabel.none))
+    engine.check(site_refines(s222, SiteLabel.none))
     engine.check(phosphorylated(MEK, serine))
-    engine.check(phosphorylated(MEK, KinaseLabel.none))
+    engine.check(phosphorylated(MEK, SiteLabel.none))
 
     # This should be unsat.
     engine.check(Not(phosphorylated(MEK, serine)))
-    engine.check(Not(phosphorylated(MEK, KinaseLabel.none)))
+    engine.check(Not(phosphorylated(MEK, SiteLabel.none)))
     engine.pop()
 
 if __name__ == "__main__":
