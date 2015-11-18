c4f84ad49bd8fe2fb0b4a1575a3110e658f0e588
Chelsea Voss <csvoss@mit.edu>
2015-11-18 16:49:57 -0500

    First four predicates from metakappa logic

--- metakappa/Model.py
+++ metakappa/Model.py
@@ -10,9 +10,10 @@ class GraphAction:
     Graph actions.
     """
     def doAction(action):
-        pass
+        raise NotImplementedError("Must implement in subclasses")
 class GraphId(GraphAction):
-    pass
+    def doAction(action):
+        pass
 class GraphUnaryAction(GraphAction):
     def __init__(a):
         self.a = a
@@ -61,6 +62,20 @@ class Node:
     def __init__(self, v):
         self.v = v
 
+
+def default_label(agent_identifier):
+    """Takes in agent identifiers, and returns an element of some alphabet.
+    For now, just return the agent identifier. It's probably a string."""
+    return agent_identifier
+
+
+# Sigma_1, Sigma_2... Sigma_n.
+default_alphabets = []
+
+# The logic is parametrized over some set of alphabets.
+alphabets = default_alphabets
+
+
 class Graph:
     """ A structured graph is a 4-tuple <V, L, P, \lambda> with nodes
     V \subseteq fancyV where:
@@ -76,14 +91,14 @@ class Graph:
         self.nodes = Set()        # list of nodes
         self.links = Set()        # list of edges
         self.bindings = Set()     # list of bindings (use dict?)
-        self.labels = []          # hm, what are labels?
+        self.label = default_label  # function which labels nodes
 
     def __eq__(self, other):
         return self.__allnodes == other.__allnodes and \
             self.nodes == other.nodes and \
             self.links == other.links and \
             self.bindings == other.bindings and \
-            self.labels == other.labels
+            self.label == other.label
 
     @staticmethod
     def unord(s):
@@ -120,7 +135,7 @@ class Model:
         """Definition 4.
         """
         newModel = Set()
-        
+
         m2_graphs = [seq[0] for seq in model2.model]
         for (graph, action) in self.model:
             if graph in m2_graphs:
@@ -132,3 +147,80 @@ class Model:
                 newModel.add((graph, action))
 
         return newModel
+
+
+class Predicate:
+    def filter_over(g_set, a_set):
+        """
+        Return a set of graphs and actions for which the predicate is true.
+
+        Arguments:
+            g_set: a set of Graphs
+            a_set: a set of Actions
+
+        Returns:
+            a set of (Graph, Action) pairs
+        """
+        assert isinstance(g_set, set)
+        for g in g_set:
+            assert isinstance(g, Graph)
+        assert isinstance(a_set, set)
+        for a in a_set:
+            assert isinstance(a, Action)
+        output = set(filter_over_helper(g_set, a_set))
+        for tup in output:
+            assert isinstance(tup, tuple) and len(tup) == 2
+            g, a = tup
+            assert isinstance(g, Graph)
+            assert isinstance(a, Action)
+        return output
+
+    def filter_over_helper(g_set, a_set):
+        raise NotImplementedError("Must implement in subclasses")
+
+    def __init__(self):
+        pass
+
+class TopPredicate(Predicate):
+    def filter_over_helper(g_set, a_set):
+        return itertools.product(g_set, a_set)
+class BottomPredicate(Predicate):
+    def filter_over_helper(g_set, a_set):
+        return set()
+class EqualsPredicate(Predicate):
+    def __init__(self, I, x, y):
+        """[x=y]_I.
+        Arguments:
+            I: the interpretation? Function from variable -> string, probably.
+            x: a variable?
+            y: a variable?
+        """
+        self.x = I(x)
+        self.y = I(y)
+    def filter_over_helper(g_set, a_set):
+        return TopPredicate().filter_over(g_set, a_set) if (self.x == self.y)
+                else BottomPredicate().filter_over(g_set, a_set)
+class SigmaPredicate(Predicate):
+    def __init__(self, I, sigma, x):
+        """[\Sigma(x)]_I.
+        Arguments:
+            I: the interpretation? Function from variable -> string, probably.
+            sigma: an alphabet?
+            x: a variable?
+        """
+        self.x = I(x)
+        self.sigma = sigma
+    def filter_over_helper(g_set, a_set):
+        for g in g_set:
+            if g.label(x) in self.sigma:
+                for a in a_set:
+                    yield (g, a)
+class LabeledPredicate(Predicate):
+    def __init__(self, I, A, x):
+        """[A(x)]_I.
+        Arguments:
+            I: the interpretation? Function from variable -> string, probably.
+            A: a label? Same type as the elements of an alphabet (a \Sigma).
+            x: a variable?
+        """
+        pass
