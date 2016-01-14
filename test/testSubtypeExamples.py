d9fcdb397090dbc6ed9c323a68c327b9a3e2d135
Jean Yang <jean.yang.writeme@gmail.com>
2016-01-14 16:45:30 -0500

    preliminary example 2

--- test/testSubtypeExamples.py
+++ test/testSubtypeExamples.py
@@ -22,3 +22,23 @@ class TestSubtypeExamples(unittest.TestCase):
         print clusters
         self.assertEqual(len(clusters), 1)
         self.assertEqual(len(clusters[0]), 2)
+
+    def test_example_2(self):
+        """
+        Reads three statements, with BRAF, ARAF, and RAF. Groups them into the
+        same cluster.
+
+        Advanced: Determines we should be able to have only RAF?
+        """
+        example = self.openFile("examples/syndra_example_2.pkl")
+        clusters = processStatements(example.statements)
+        print clusters
+        self.assertEqual(len(clusters), 1)
+        self.assertEqual(len(clusters[0]), 3)
+        # TODO: The advanced stuff
+
+    def test_example_3(self):
+        example = self.openFile("examples/syndra_example_3.pkl")
+
+    def test_example_4(self):
+        example = self.openFile("examples/syndra_example_4.pkl")
