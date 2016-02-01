a0c7a92c55469ecef32597dc067d66b76fbef5e5
Chelsea Voss <csvoss@mit.edu>
2016-02-01 05:45:13 -0500

    Convert everything in model.py to being a Z3 datatype

--- l_to_z3/model.py
+++ l_to_z3/model.py
@@ -5,11 +5,6 @@ Krivine - using the Python bindings for Z3.
 from z3 import *
 from z3_helpers import *
 
-# TODO: Make everything in this file a z3 datatype with some helper functions
-# lying around; make nothing a Python class. It's easier to keep them
-# all in order that way.
-
-# Then, it's easier to implement some of the remaining Predicates.
 
 # Node is a datatype representing a vertex or node in a Kappa graph.
 Node = Datatype('Node')
@@ -17,11 +12,12 @@ Node.declare('node', ('label', IntSort()))
 Node = Node.create()
 
 # Graph, before a rule or action has applied.
-class Pregraph(object):
-    def __init__(self):
-        self.has = Function('pregraph_has', Identifier, BoolSort())
-        self.links = Function('pregraph_links', Identifier, Identifier, BoolSort())
-        self.parents = Function('pregraph_parents', Identifier, Identifier, BoolSort())
+Pregraph = Datatype('Pregraph')
+Pregraph.declare('pregraph',
+    ('pregraph_has', Function('f1', Node, BoolSort())),
+    ('pregraph_links', Function('f2', Node, Node, BoolSort())),
+    ('pregraph_parents', Function('f3', Node, Node, BoolSort())))
+Pregraph = Pregraph.create()
 
 # Atomic action. An Action is comprised of a set of these.
 AtomicAction = Datatype('AtomicAction')
@@ -39,68 +35,56 @@ AtomicAction.declare('unparent_action',
 AtomicAction = AtomicAction.create()
 
 # Action: a set of atomic actions.
-class Action(object):
-    def __init__(self):
-        self.has = Function('action_has', AtomicAction, BoolSort())
+Action = Function('action_has', AtomicAction, BoolSort())
 
 # Graph, after a rule or action has been applied.
-class Postgraph(object):
-    def __init__(self, graph, action):
-        self.has = Function('postgraph_has', Node, BoolSort())
-        self.links = Function('postgraph_links', Node, Node, BoolSort())
-        self.parents = Function('postgraph_parents', Node, Node, BoolSort())
+Postgraph = Datatype('Postgraph')
+Postgraph.declare('postgraph',
+    ('postgraph_has', Function('f4', Node, BoolSort())),
+    ('postgraph_links', Function('f5', Node, Node, BoolSort())),
+    ('postgraph_parents', Function('f6', Node, Node, BoolSort())))
+Postgraph = Postgraph.create()
 
-        # Constrain the postgraph's nodes, links, and parent-child relationships
-        # appropriately, according to what the graph and action contain.
-        i = Const('i', Node)
-        j = Const('j', Node)
+def postgraph_constraints(pregraph, action, postgraph):
+    """
+    Create a list of assertions that constrain the postgraph's nodes, links, and
+    parent-child relationships appropriately, according to what the graph and
+    action contain.
+    """
+    i = Const('i', Node)
+    j = Const('j', Node)
 
-        self.assertions = [
-            ForAll(i,
-                    Iff(self.has(i),
-                        Or(And(graph.has(i), Not(action.has(AtomicAction.rem_action(i)))),
-                           action.has(AtomicAction.add_action(i))))),
-            ForAll([i, j],
-                Iff(self.links(i, j),
-                    Or(
+    assertions = [
+        ForAll(i,
+                Iff(postgraph.has(i),
+                    Or(And(graph.has(i), Not(action.has(AtomicAction.rem_action(i)))),
+                       action.has(AtomicAction.add_action(i))))),
+        ForAll([i, j],
+            Iff(self.links(i, j),
+                Or(
+                    And(
                         And(
-                            And(
-                                graph.links(i, j),
-                                Not(action.has(AtomicAction.unlink_action(i, j)))),
-                            And(
-                                Not(action.has(AtomicAction.rem_action(i))),
-                                Not(action.has(AtomicAction.rem_action(j))))),
-                        action.has(AtomicAction.link_action(i, j))))),
-            ForAll([i, j],
-                Iff(self.parents(i, j),
-                    Or(
+                            graph.links(i, j),
+                            Not(action.has(AtomicAction.unlink_action(i, j)))),
                         And(
-                            And(
-                                graph.parents(i, j),
-                                Not(action.has(AtomicAction.unparent_action(i, j)))),
-                            And(
-                                Not(action.has(AtomicAction.rem_action(i))),
-                                Not(action.has(AtomicAction.rem_action(j))))),
-                            action.has(AtomicAction.parent_action(i, j)))))
-        ]
+                            Not(action.has(AtomicAction.rem_action(i))),
+                            Not(action.has(AtomicAction.rem_action(j))))),
+                    action.has(AtomicAction.link_action(i, j))))),
+        ForAll([i, j],
+            Iff(postgraph.parents(i, j),
+                Or(
+                    And(
+                        And(
+                            graph.parents(i, j),
+                            Not(action.has(AtomicAction.unparent_action(i, j)))),
+                        And(
+                            Not(action.has(AtomicAction.rem_action(i))),
+                            Not(action.has(AtomicAction.rem_action(j))))),
+                        action.has(AtomicAction.parent_action(i, j)))))
+    ]
 
+    return assertions
 
-# This is Kappa's <graph, action> pair, but I've included the postgraph
-# (i.e. the graph after applying the action), too, for convenience.
-class GraphActionPair(object):
-    def __init__(self):
-        self.pregraph = Pregraph()
-        self.action = Action()
-        self.postgraph = Postgraph(self.pregraph, self.action)
-
-    def initialize(self, solver):
-        """Add, to some z3 solver, the necessary assertions to create
-        an instance of this model.
-
-        For now, the only such assertions needed are in the postgraph."""
-
-        for assertion in self.postgraph.assertions:
-            solver.add(assertion)
 
 # This represents a set of possible <graph, action> pairs -- a single Kappa
 # chemical system.
@@ -112,5 +96,4 @@ ChemicalSystem = ChemicalSystem.create()
 
 # This represents a set of sets of <graph, action> pairs -- many possible
 # chemical systems that an L statement could represent.
-def new_model():
-    return Function('contains', ChemicalSystem, BoolSort())
+ChemicalSystemSet = Function('possible_system', ChemicalSystem, BoolSort())
