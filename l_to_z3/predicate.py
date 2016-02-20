8eca9719d5be5f4d820daddc8289c6b5b2787f75
Chelsea Voss <csvoss@mit.edu>
2016-02-19 23:51:37 -0500

    Make notes of what classes to include in predicate, from test_predicate

---
+++ l_to_z3/predicate.py
@@ -0,0 +1,26 @@
+class Predicate(object):
+    pass
+
+class And(object):
+    pass
+
+class Or(object):
+    pass
+
+class Join(object):
+    pass
+
+class DontKnow(object):
+    pass
+
+class Not(object):
+    pass
+
+
+
+# From atomic_predicate, must import or re-wrap at least the following:
+# Labeled
+# PreParent
+# DoLink
+# Top
+# Bottom
