6644873463b7bed45695213f3145607f38e8e54d
Chelsea Voss <csvoss@mit.edu>
2016-03-01 02:00:40 -0500

    Pseudocode for macros for phosphorylates, activates

--- l_to_z3/macros.py
+++ l_to_z3/macros.py
@@ -1,8 +1,14 @@
-# Macro for "A phosphorylates B"
-# TODO
+# Macro for "A" phosphorylates "B"
+# forall A. Named("A", A) => forall B. Named("B", B) =>
+#     (Labeled(A, "Active") /\ Unlabeled(B, "Phosphorylated") /\
+#     PostLabeled(A, "Active") /\ PostLabeled(B, "Phosphorylated"))
 
-# Macro for "Phosphorylated B is active"
-# TODO
+# Macro for Phosphorylated "B" is active
+# forall B. Named("B", B) =>
+#     (PreLabeled(B, "Phosphorylated") => PreLabeled(B, "Active")) and
+#     (PostLabeled(B, "Phosphorylated") => PostLabeled(B, "Active"))
 
-# Macro for "A activates B"
-# TODO
+# Macro for "A" activates "B"
+# forall A. Named("A", A) => forall B. Named("B", B) =>
+#     (Labeled(A, "Active") /\ Unlabeled(B, "Active") /\
+#     PostLabeled(A, "Active") /\ PostLabeled(B, "Active"))
