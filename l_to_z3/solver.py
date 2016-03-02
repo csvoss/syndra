586748dc772e5118e3049ae8699a12528e8eb5af
Chelsea Voss <csvoss@mit.edu>
2016-03-02 14:27:34 -0500

    Solver class in solver.py

--- l_to_z3/solver.py
+++ l_to_z3/solver.py
@@ -5,9 +5,8 @@ Global Z3 solver for use by other files.
 from contextlib import contextmanager
 from z3 import Solver
 
-global_solver = Solver()
 
-class Solver(object):
+class MySolver(object):
 
     def __init__(self):
         self._solver = Solver()
@@ -15,49 +14,52 @@ class Solver(object):
 
     # TODO: Port the below functions to here as methods
 
-def push():
-    """Push solver state."""
-    global_solver.push()
+    def push(self):
+        """Push solver state."""
+        self._solver.push()
 
-def pop():
-    """Pop solver state."""
-    global_solver.pop()
+    def pop(self):
+        """Pop solver state."""
+        self._solver.pop()
 
-def add(assertion):
-    """Add an assertion to the solver state.
+    def add(self, assertion):
+        """Add an assertion to the solver state.
 
-    Arguments:
-        assertion : Z3-friendly predicate or boolean
-    """
-    return global_solver.add(assertion)
+        Arguments:
+            assertion : Z3-friendly predicate or boolean
+        """
+        return self._solver.add(assertion)
 
-def model():
-    """Return a model for the current solver state.
+    def model(self):
+        """Return a model for the current solver state.
 
-    Returns:
-        : Z3 model. TODO: Modify this all so that it returns sets, etc.
-    """
-    return global_solver.model()
+        Returns:
+            : Z3 model. TODO: Modify this all so that it returns sets, etc.
+        """
+        return self._solver.model()
 
-def check():
-    """Check satisfiability of current satisfiability state.
+    def check(self):
+        """Check satisfiability of current satisfiability state.
 
-    Returns:
-        : boolean -- True if sat, False if unsat
-    """
-    # check() returns either unsat or sat
-    # sat.r is 1, unsat.r is -1
-    return global_solver.check().r > 0
+        Returns:
+            : boolean -- True if sat, False if unsat
+        """
+        # check() returns either unsat or sat
+        # sat.r is 1, unsat.r is -1
+        return self._solver.check().r > 0
 
-@contextmanager
-def context():
-    """To do something in between a push and a pop, use a `with context()`."""
-    push()
-    yield
-    pop()
+    @contextmanager
+    def context(self):
+        """To do something in between a push and a pop, use a `with context()`."""
+        self.push()
+        yield
+        self.pop()
 
-def quick_check(assertion):
-    """Add an assertion only temporarily, and check sat."""
-    with context():
-        add(assertion)
-        return check()
+    def quick_check(self, assertion):
+        """Add an assertion only temporarily, and check sat."""
+        with self.context():
+            self.add(assertion)
+            return self.check()
+
+
+solver = MySolver()
