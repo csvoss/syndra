6a04120096bc0b8ce4045b07f9b473a524b95ccd
Chelsea Voss <csvoss@mit.edu>
2016-03-07 14:15:50 -0500

    Added atomic_predicate Equals test

--- l_to_z3/test_atomic_predicate.py
+++ l_to_z3/test_atomic_predicate.py
@@ -1,3 +1,5 @@
+import z3
+
 from unittest import TestCase
 import atomic_predicate
 import datatypes
@@ -10,17 +12,53 @@ class AtomicPredicateTestCase(TestCase):
         self.submodel = datatypes.new_model()
         self.interpretation = datatypes.new_interpretation()
 
+    def assertSat(self, pred):
+        asserted = pred._assert(self.submodel, self.interpretation)
+        check = solver.quick_check(asserted)
+        self.assertTrue(check)
+
+    def assertUnsat(self, pred):
+        asserted = pred._assert(self.submodel, self.interpretation)
+        check = solver.quick_check(asserted)
+        self.assertFalse(check)
+
     def test_top_sat(self):
-        top = atomic_predicate.Top()
-        predicate = top._assert(self.submodel, self.interpretation)
-        result = solver.quick_check(predicate)
-        self.assertEquals(result, True)
+        pred = atomic_predicate.Top()
+        self.assertSat(pred)
 
     def test_bottom_unsat(self):
-        bottom = atomic_predicate.Bottom()
-        predicate = bottom._assert(self.submodel, self.interpretation)
-        result = solver.quick_check(predicate)
-        self.assertEquals(result, False)
+        pred = atomic_predicate.Bottom()
+        self.assertUnsat(pred)
+
+    def test_equal_sat(self):
+        v1 = datatypes.new_variable()
+        v2 = datatypes.new_variable()
+        pred = atomic_predicate.Equal(v1, v2)
+        self.assertSat(pred)
+
+    def test_equal_transitive(self):
+        v1 = datatypes.new_variable()
+        v2 = datatypes.new_variable()
+        v3 = datatypes.new_variable()
+        pred1 = atomic_predicate.Equal(v1, v2)
+        pred2 = atomic_predicate.Equal(v2, v3)
+        pred3 = atomic_predicate.Equal(v1, v3)
+
+        solver.push()
+
+        solver.add(pred1._assert(self.submodel, self.interpretation))
+        solver.add(pred2._assert(self.submodel, self.interpretation))
+        sat = solver.check()
+        self.assertTrue(sat)
+
+        solver.add(z3.Not(pred3._assert(self.submodel, self.interpretation)))
+        unsat = solver.check()
+        self.assertFalse(unsat)
+
+        solver.pop()
+
+    
+
 
 
     # TODO: tests for Or, Join, And, DontKnow, Not, Forall, Exists
