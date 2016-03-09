cbcdef2b8cd7f4070abc2258ac5238203df54527
Chelsea Voss <csvoss@mit.edu>
2016-03-09 16:26:14 -0500

    Make string interner's attributes private

--- l_to_z3/string_interner.py
+++ l_to_z3/string_interner.py
@@ -1,23 +1,23 @@
 class StringInterner(object):
 
     def __init__(self):
-        self.str_to_int = {}
-        self.int_to_str = {}
+        self._str_to_int = {}
+        self._int_to_str = {}
         self.counter = 1
 
     def add(self, string):
-        self.str_to_int[string] = self.counter
-        self.int_to_str[self.counter] = string
+        self._str_to_int[string] = self.counter
+        self._int_to_str[self.counter] = string
         self.counter += 1
 
     def has_string(self, string):
-        return string in self.str_to_int
+        return string in self._str_to_int
 
     def get_int(self, string):
-        return self.str_to_int[string]
+        return self._str_to_int[string]
 
     def get_str(self, integer):
-        return self.int_to_str[integer]
+        return self._int_to_str[integer]
 
     def get_int_or_add(self, string):
         if not self.has_string(string):
