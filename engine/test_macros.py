5b8d4292c81a8d674da410b66b4ea0bb3a888c1c
Chelsea Voss <csvoss@mit.edu>
2016-03-21 21:59:59 -0400

    Add print statements to test_macros.py

--- engine/test_macros.py
+++ engine/test_macros.py
@@ -9,31 +9,34 @@ from predicate import Implies, And, Not
 from solver import solver
 import z3
 
+print "Predicate 1: MEK directly phosphorylates ERK in a single step"
 i = directly_phosphorylates("MEK", "ERK")
+
+print "Predicate 2: ERK, when phosphorylated, is always active"
 ii = phosphorylated_is_active("ERK")
+
+print "Predicate 3: MEK directly activates ERK in a single step"
 iii = directly_activates("MEK", "ERK")
 
+print "\nEach of these predicates is satisfiable (True) on their own:"
 assert solver.quick_check(i)
+print solver.quick_check(i)
 assert solver.quick_check(ii)
+print solver.quick_check(ii)
 assert solver.quick_check(iii)
+print solver.quick_check(iii)
 
+print "\n1 and 2 are satisfiable together:"
 and_i_ii = z3.And(i, ii)
 assert solver.quick_check(and_i_ii)
+print solver.quick_check(and_i_ii)
 
+print "\n1 and 2 imply 3:"
 implication = z3.Implies(and_i_ii, iii)
 assert solver.quick_check(implication)
+print solver.quick_check(implication)
 
+print "\n1 and 2 and 3 are satisfiable together:"
 and_i_ii_iii = z3.And(i, ii, iii)
 assert solver.quick_check(and_i_ii_iii)
-
-
-# indirectly_activates
-
-# be sure to address in thesis: why not just straight inference rules?
-# (answer: easy to introduce inconsistencies, use different definitions
-# across different rules)
-
-
-# x: A activates B
-# y: B activates C
-# z: A activates C
+print solver.quick_check(and_i_ii_iii)
