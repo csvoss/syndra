623a50018bb7de1ac813e5748c8bb4fc28bd705a
Chelsea Voss <csvoss@mit.edu>
2016-03-21 21:42:40 -0400

    Clean up test_macros.py

--- engine/macros.py
+++ engine/macros.py
@@ -1,5 +1,9 @@
-from predicate import PreLabeled, PreUnlabeled, Named, PostLabeled, And, Implies, ForAll
+import atomic_predicate
+import datatypes
 from datatypes import new_variable
+import predicate
+from predicate import PreLabeled, PreUnlabeled, Named, PostLabeled, And, Implies, ForAll
+import z3
 
 from labels import ACTIVE, PHOSPHORYLATED
 
@@ -14,6 +18,20 @@ def directly_phosphorylates(name_a, name_b):
     # Forall Model, Exists Rule in Model, reaction stuff holds over rule
     # reaction stuff = Forall A B, Named A name_a /\ Named B name_b => prelabeled etc
 
+    g = datatypes.new_graph('g')
+    a = datatypes.new_action('a')
+    intp = datatypes.new_interpretation()
+    subm = predicate.model_from(g, a)
+
+    # Manual implementation
+    return z3.Exists([g, a], z3.Implies(atomic_predicate.Named(A, name_a)._assert(subm, intp),
+            z3.Implies(atomic_predicate.Named(B, name_b)._assert(subm, intp),
+                z3.And(atomic_predicate.PreLabeled(A, ACTIVE)._assert(subm, intp),
+                    atomic_predicate.PreUnlabeled(B, PHOSPHORYLATED)._assert(subm, intp),
+                    atomic_predicate.PostLabeled(A, ACTIVE)._assert(subm, intp),
+                    atomic_predicate.PostLabeled(B, PHOSPHORYLATED)._assert(subm, intp)))))
+
+    # Old implementation
     return ForAll(A, ForAll(B,
                Implies(Named(A, name_a), Implies(Named(B, name_b),
                    And(PreLabeled(A, ACTIVE),
@@ -28,6 +46,20 @@ def phosphorylated_is_active(name_b):
     """
     # Forall Model, *Forall* Rule in Model, Forall B, Named B named_b => and(...)
     B = new_variable(nickname=name_b)
+
+    g = datatypes.new_graph('g')
+    a = datatypes.new_action('a')
+    intp = datatypes.new_interpretation()
+    subm = predicate.model_from(g, a)
+
+    # Manual implementation
+    return z3.ForAll([g, a], z3.Implies(atomic_predicate.Named(B, name_b)._assert(subm, intp),
+        z3.And(z3.Implies(atomic_predicate.PreLabeled(B, PHOSPHORYLATED)._assert(subm, intp),
+                    atomic_predicate.PreLabeled(B, ACTIVE)._assert(subm, intp)),
+            z3.Implies(atomic_predicate.PostLabeled(B, PHOSPHORYLATED)._assert(subm, intp),
+                    atomic_predicate.PostLabeled(B, ACTIVE)._assert(subm, intp)))))
+
+    # Old
     return ForAll(B, Implies(Named(B, name_b),
                And(Implies(PreLabeled(B, PHOSPHORYLATED),
                            PreLabeled(B, ACTIVE)),
@@ -42,6 +74,21 @@ def directly_activates(name_a, name_b):
     # Forall Model, Exists Rule in Model, [...]
     A = new_variable(nickname=name_a)
     B = new_variable(nickname=name_b)
+
+    g = datatypes.new_graph('g')
+    a = datatypes.new_action('a')
+    intp = datatypes.new_interpretation()
+    subm = predicate.model_from(g, a)
+
+    # Manual implementation
+    return z3.Exists([g, a], z3.Implies(atomic_predicate.Named(A, "MEK")._assert(subm, intp),
+            z3.Implies(atomic_predicate.Named(B, "ERK")._assert(subm, intp),
+                z3.And(atomic_predicate.PreLabeled(A, ACTIVE)._assert(subm, intp),
+                    atomic_predicate.PreUnlabeled(B, ACTIVE)._assert(subm, intp),
+                    atomic_predicate.PostLabeled(A, ACTIVE)._assert(subm, intp),
+                    atomic_predicate.PostLabeled(B, ACTIVE)._assert(subm, intp)))))
+
+    # Old
     return ForAll(A, ForAll(B,
                Implies(Named(A, name_a), Implies(Named(B, name_b),
                    And(PreLabeled(A, ACTIVE),
--- engine/test_macros.py
+++ engine/test_macros.py
@@ -13,73 +13,19 @@ i = directly_phosphorylates("MEK", "ERK")
 ii = phosphorylated_is_active("ERK")
 iii = directly_activates("MEK", "ERK")
 
+assert solver.quick_check(i)
+assert solver.quick_check(ii)
+assert solver.quick_check(iii)
 
-# Manual Z3 translation of first predicate
-g = datatypes.new_graph('g')
-a = datatypes.new_action('a')
-A = datatypes.new_variable(nickname="MEK")
-B = datatypes.new_variable(nickname="ERK")
-intp = datatypes.new_interpretation()
-subm = predicate.model_from(g, a)
+and_i_ii = z3.And(i, ii)
+assert solver.quick_check(and_i_ii)
 
-I = z3.Exists([g, a], z3.Implies(atomic_predicate.Named(A, "MEK")._assert(subm, intp),
-        z3.Implies(atomic_predicate.Named(B, "ERK")._assert(subm, intp),
-            z3.And(atomic_predicate.PreLabeled(A, ACTIVE)._assert(subm, intp),
-                atomic_predicate.PreUnlabeled(B, PHOSPHORYLATED)._assert(subm, intp),
-                atomic_predicate.PostLabeled(A, ACTIVE)._assert(subm, intp),
-                atomic_predicate.PostLabeled(B, PHOSPHORYLATED)._assert(subm, intp)))))
-
-assert solver.quick_check(I)
-
-# Manual Z3 predicate of second predicate
-A = datatypes.new_variable(nickname="MEK")
-intp = datatypes.new_interpretation()
-subm = predicate.model_from(g, a)
-
-II = z3.ForAll([g, a], z3.Implies(atomic_predicate.Named(A, "MEK")._assert(subm, intp),
-    z3.And(z3.Implies(atomic_predicate.PreLabeled(B, PHOSPHORYLATED)._assert(subm, intp),
-                atomic_predicate.PreLabeled(B, ACTIVE)._assert(subm, intp)),
-        z3.Implies(atomic_predicate.PostLabeled(B, PHOSPHORYLATED)._assert(subm, intp),
-                atomic_predicate.PostLabeled(B, ACTIVE)._assert(subm, intp)))))
-
-assert solver.quick_check(II)
-
-# Manual Z3 translation of third predicate
-g = datatypes.new_graph('g')
-a = datatypes.new_action('a')
-A = datatypes.new_variable(nickname="MEK")
-B = datatypes.new_variable(nickname="ERK")
-intp = datatypes.new_interpretation()
-subm = predicate.model_from(g, a)
-
-III = z3.Exists([g, a], z3.Implies(atomic_predicate.Named(A, "MEK")._assert(subm, intp),
-        z3.Implies(atomic_predicate.Named(B, "ERK")._assert(subm, intp),
-            z3.And(atomic_predicate.PreLabeled(A, ACTIVE)._assert(subm, intp),
-                atomic_predicate.PreUnlabeled(B, ACTIVE)._assert(subm, intp),
-                atomic_predicate.PostLabeled(A, ACTIVE)._assert(subm, intp),
-                atomic_predicate.PostLabeled(B, ACTIVE)._assert(subm, intp)))))
-
-assert solver.quick_check(III)
-
-and_I_II = z3.And(I, II)
-assert solver.quick_check(and_I_II)
-
-implication = z3.Implies(z3.And(I, II), III)
+implication = z3.Implies(and_i_ii, iii)
 assert solver.quick_check(implication)
 
+and_i_ii_iii = z3.And(i, ii, iii)
+assert solver.quick_check(and_i_ii_iii)
 
-# # Implies(And(i, ii), iii): check a theorem over all possible models
-# pred1 = Implies(And(i, ii), iii)
-# assert pred1.check_sat() # This should be true.
-# pred2 = Not(Implies(And(i, ii), iii))
-# assert pred2.check_sat() # This should be false.
-#
-# pred3 = And(i, ii, iii)
-# assert pred3.check_sat() # This should be true.
-# pred4 = Not(And(i, ii, iii))
-# assert pred4.check_sat() # This should be true.
-
-# inconsistencies in a model: And(i, ii, iii) and see what happens
 
 # indirectly_activates
 
