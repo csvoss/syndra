b823577478a8e35f5ec5de0d7b9bcc805061efbb
Chelsea Voss <csvoss@mit.edu>
2016-03-21 22:23:53 -0400

    Display predicates in test_macros.py; add negative example

--- engine/test_macros.py
+++ engine/test_macros.py
@@ -7,16 +7,19 @@ from predicate import Implies, And, Not
 from solver import solver
 import z3
 
-print "Predicate 1: MEK directly phosphorylates ERK in a single step"
+print "Predicate I: MEK directly phosphorylates ERK in a single step.\nTranslated to z3:"
 i = directly_phosphorylates("MEK", "ERK")
+print i
 
-print "Predicate 2: ERK, when phosphorylated, is always active"
+print "\nPredicate II: ERK, when phosphorylated, is always active.\nTranslated to z3:"
 ii = phosphorylated_is_active("ERK")
+print ii
 
-print "Predicate 3: MEK directly activates ERK in a single step"
+print "\nPredicate III: MEK directly activates ERK in a single step.\nTranslated to z3:"
 iii = directly_activates("MEK", "ERK")
+print iii
 
-print "\nEach of these predicates is satisfiable (True) on their own:"
+print "\nEach of these predicates is satisfiable (True) on their own: checking..."
 assert solver.quick_check(i)
 print solver.quick_check(i)
 assert solver.quick_check(ii)
@@ -24,17 +27,21 @@ print solver.quick_check(ii)
 assert solver.quick_check(iii)
 print solver.quick_check(iii)
 
-print "\n1 and 2 are satisfiable together:"
+print "\nI and II are satisfiable together: checking And(I, II)..."
 and_i_ii = z3.And(i, ii)
 assert solver.quick_check(and_i_ii)
 print solver.quick_check(and_i_ii)
 
-print "\n1 and 2 imply 3:"
+print "\nI and II imply III: checking Implies(And(I, II), III)..."
 implication = z3.Implies(and_i_ii, iii)
 assert solver.quick_check(implication)
 print solver.quick_check(implication)
 
-print "\n1 and 2 and 3 are satisfiable together:"
+print "\nI and II and III are satisfiable together: checking And(I, II, III)..."
 and_i_ii_iii = z3.And(i, ii, iii)
 assert solver.quick_check(and_i_ii_iii)
 print solver.quick_check(and_i_ii_iii)
+
+print "\nI and not-I are NOT satisfiable together: checking And(I, Not(I))..."
+assert not solver.quick_check(z3.And(z3.Not(i), i))
+print solver.quick_check(z3.And(z3.Not(i), i))
