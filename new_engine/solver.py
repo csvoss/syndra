b1e58a82be9fe196a2272442433e6f388acf41be
Chelsea Voss <csvoss@mit.edu>
2016-07-19 16:40:29 -0400

    Pylint solver.py

--- new_engine/solver.py
+++ new_engine/solver.py
@@ -1,12 +1,22 @@
 """
-Global Z3 solver for use by other files.
+Solver for reasoning about Syndra predicates.
 """
 from contextlib import contextmanager
 import z3
 
 import predicate
+import pythonize
 
 class MySolver(object):
+    """
+    This class allows you to create instances of solvers, which manage their
+    own state. Solvers are for reasoning about the satisfiability of
+    Syndra predicates.
+
+    The design of this solver is inspired by the methods provided by z3's
+    solver.
+    """
+
     def __init__(self):
         self._solver = z3.Solver()
 
@@ -19,7 +29,7 @@ class MySolver(object):
         self._solver.pop()
 
     def add(self, syndra_predicate):
-        """Add an assertion to the solver state.
+        """Add a Syndra predicate to the solver state.
 
         Arguments:
             syndra_predicate : Syndra predicate, instance of predicate.Predicate
@@ -27,8 +37,8 @@ class MySolver(object):
         z3_predicate = syndra_predicate.get_predicate()
         self._solver.add(z3_predicate)
 
-    def add_z3(self, z3_predicate):
-        """Add an assertion to the solver state.
+    def _add_z3(self, z3_predicate):
+        """Add a z3 predicate to the solver state.
 
         Arguments:
             z3_predicate : Z3-friendly predicate or boolean
@@ -38,8 +48,15 @@ class MySolver(object):
     def model(self):
         """Return a model for the current solver state.
 
-        Returns:
-            : Z3 model.
+        Returns :: set of RuleResult objects
+        """
+        model = self._model_z3()
+        return pythonize.pythonized(self, model) # TODO: inline it here
+
+    def _model_z3(self):
+        """Return a model for the current solver state, straight from z3.
+
+        Returns :: Z3 model.
         """
         z3model = self._solver.model()
         return z3model
@@ -47,13 +64,17 @@ class MySolver(object):
     def check(self):
         """Check satisfiability of current satisfiability state.
 
-        Returns:
-            : boolean -- True if sat, False if unsat
+        Returns :: boolean -- True if sat, False if unsat
         """
         # check() returns either unsat or sat
         result = self._solver.check()
         # sat.r is 1, unsat.r is -1
-        return result.r > 0
+        if result.r > 0:
+            return True
+        if result.r < 0:
+            return False
+        else:
+            raise ValueError("z3 returned unknown")
 
     @contextmanager
     def context(self):
@@ -65,20 +86,19 @@ class MySolver(object):
         self.pop()
 
     def quick_check(self, syndra_predicate):
-        """Add an assertion only temporarily, and check sat."""
+        """Check if a predicate is satisfiable without changing solver state."""
         with self.context():
             self.add(syndra_predicate)
             return self.check()
 
     def quick_check_sat(self, syndra_predicate):
+        """See the documentation for quick_check(...)."""
         return self.quick_check(syndra_predicate)
 
     def quick_check_valid(self, syndra_predicate):
-        """Check that a predicate is valid - that its negation is unsat."""
+        """Quickcheck that a predicate is valid - that its negation is unsat."""
         with self.context():
             if not self.check():
                 return True # The predicate is implied by the unsat env
             self.add(predicate.Not(syndra_predicate))
             return not self.check() # if negation is unsat, then it's valid
-
-solver = MySolver()
