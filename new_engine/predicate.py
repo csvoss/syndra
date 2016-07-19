d697e39c5062ef24ea3d79071350659b0ac649dc
Chelsea Voss <csvoss@mit.edu>
2016-07-19 16:22:41 -0400

    Pylint predicate.py

--- new_engine/predicate.py
+++ new_engine/predicate.py
@@ -1,20 +1,27 @@
+"""
+predicate.py stores a number of classes subclassing Predicate, the base
+class of any Syndra predicate.
+"""
 import z3
 
-from pythonize import pythonized
-from solver import solver
 import datatypes
-from datatypes import new_node
-from string_interner import string_interner, node_interner
 
 class Predicate(object):
-    def __init__(self):
-        raise NotImplementedError("Predicate is an abstract class.")
+    """An abstract class for defining Syndra predicates."""
+    def get_predicate(self, model_variable):
+        """Converts this Syndra predicate into a z3 predicate.
 
-    def _get_predicate(self, model_variable):
-        # returns a z3 predicate
-        predicate = self._assert(self.model_variable)
+        Called by the solver in solver.py; should only be used there. Interact
+        with Syndra predicates by adding them to the solver -- consult the
+        documentation in solver.py.
+
+        Arguments:
+            model_variable :: z3 function from Rule to BoolSort
+
+        Returns :: z3 predicate.
+        """
+        predicate = self._assert(model_variable)
         return predicate
-
     def _assert(self, model):
         raise NotImplementedError("Implement _assert in subclasses.")
 
@@ -26,70 +33,87 @@ def _ensure_predicate(thing):
 
 
 class And(Predicate):
+    """Apply logical 'and' to one or more Syndra predicates."""
     def __init__(self, *preds):
         for pred in preds:
             _ensure_predicate(pred)
         self.preds = preds
-
     def _assert(self, model):
-        return z3.And(*map(lambda p: p._assert(model),
-                           self.preds))
+        return z3.And(*(p._assert(model) for p in self.preds))
+
 
 class Not(Predicate):
+    """Apply logical 'not' to a Syndra predicate."""
     def __init__(self, pred):
         _ensure_predicate(pred)
         self.pred = pred
-
     def _assert(self, model):
         return z3.Not(self.pred._assert(model))
 
+
 class Or(Predicate):
+    """Apply logical 'or' to one or more Syndra predicates."""
     def __init__(self, *preds):
         for pred in preds:
             _ensure_predicate(pred)
         self.preds = preds
-
     def _assert(self, model):
-        return z3.Or(*map(lambda p: p._assert(model),
-                          self.preds))
+        return z3.Or(*(p._assert(model) for p in self.preds))
 
 
 class ModelHasRule(Predicate):
+    """Claims that a model has a rule satisfying the given properties.
+
+    Constructor arguments:
+        rule_function :: Rule -> Predicate
+
+    For example, if I want to assert that a model has a simple rule that only
+    needs to have MEK on the left hand side:
+
+    ModelHasRule(lambda r: And(PregraphHas(r, kinase)))
+
+    TODO: make the "lambda r" part no longer necessary by coding
+    PregraphHas and PostgraphHas to auto-insert the rule they need
+    """
     def __init__(self, rule_function):
         self.rule_function = rule_function
         self.rule_variable = None
-
     def _assert(self, model):
         self.rule_variable = datatypes.new_rule()
         return z3.Exists([self.rule_variable],
-            z3.And(model(self.rule_variable),
-            self.rule_function(self.rule_variable)
-                    ._assert(model)))
+                         z3.And(model(self.rule_variable),
+                                self.rule_function(self.rule_variable)
+                                ._assert(model)))
+
 
 class PregraphHas(Predicate):
+    """Claims that some rule's pregraph has a given structure."""
     def __init__(self, rule, structure):
         self.rule = rule
         self.structure = structure
-
     def _assert(self, model):
         return self.structure._assert(datatypes.Rule.pregraph(self.rule))
 
+
 class PostgraphHas(Predicate):
+    """Claims that some rule's postgraph has a given structure."""
     def __init__(self, rule, structure):
         self.rule = rule
         self.structure = structure
-
     def _assert(self, model):
         return self.structure._assert(datatypes.Rule.postgraph(self.rule))
 
 
 class Top(Predicate):
+    """Syndra predicate that is always satisfiable."""
     def __init__(self):
         pass
     def _assert(self, model):
         return z3.Or(True)
 
+
 class Bottom(Predicate):
+    """Syndra predicate that is never satisfiable."""
     def __init__(self):
         pass
     def _assert(self, model):
