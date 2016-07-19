98b009f32ddf35d1fe28857cee3604ed16155d1e
Chelsea Voss <csvoss@mit.edu>
2016-07-19 16:01:55 -0400

    predicate.py redesign: deprecate get_model, get_predicate, check_sat, get_python_model
    These functionalities should now be handled by adding a Syndra predicate to the
    Syndra solver in solver.py. The solver will be responsible for converting
    Syndra predicates to z3 predicates by calling the appropriate visitor patterns
    on the Syndra predicate.

--- new_engine/predicate.py
+++ new_engine/predicate.py
@@ -3,89 +3,27 @@ import z3
 from pythonize import pythonized
 from solver import solver
 import datatypes
+from datatypes import new_node
 from string_interner import string_interner, node_interner
 
 class Predicate(object):
     def __init__(self):
         raise NotImplementedError("Predicate is an abstract class.")
 
-    # TODO deprecate get_model, get_python_model, check_sat. These should
-    # all be managed by the Syndra solver (solver.py) instead.
-
-    def get_model(self):
-        with solver.context():
-            solver.add(self)
-            if not solver.check():
-                raise ValueError("Tried to get model of unsat predicate")
-
-            print pythonized(self, solver.model())
-
-            # MODEL PRUNING: EDGES
-            # For each edge in each graph, assert that that edge isn't there.
-            # If we're still satisfiable, continue. Else, pop.
-            mo = solver.model()
-            rules = [i[0] for i in mo[self.model_variable].as_list()[:-1]]
-
-            agents = node_interner._str_to_node.values()
-
-            edge_assertions = []
-            for rule in rules:
-                prg = datatypes.Rule.pregraph(rule)
-                pog = datatypes.Rule.postgraph(rule)
-                prlinks = datatypes.Graph.links(prg)
-                prparents = datatypes.Graph.parents(prg)
-                polinks = datatypes.Graph.links(pog)
-                poparents = datatypes.Graph.parents(pog)
-                for agent_1 in agents:
-                    for agent_2 in agents:
-                        edge = datatypes.Edge.edge(agent_1, agent_2)
-                        edge_assertions.append(z3.Not(z3.Select(prparents, edge)))
-                        edge_assertions.append(z3.Not(z3.Select(poparents, edge)))
-                        edge_assertions.append(z3.Not(z3.Select(prlinks, edge)))
-                        edge_assertions.append(z3.Not(z3.Select(polinks, edge)))
-
-
-            for edge_assertion in edge_assertions:
-                solver.push()
-                solver.add_z3(edge_assertion)
-                satisfiable = solver.check()
-                solver.pop()
-                if satisfiable:
-                    solver.add_z3(edge_assertion)
-
-            # MODEL PRUNING: LABELS
-            # TODO
-
-            # MODEL PRUNING: NODES
-            # TODO
-
-            assert solver.check()
-            return solver.model()
-
-    def get_python_model(self):
-        model = self.get_model()
-        return pythonized(self, model)
-
-    def check_sat(self):
-        return solver.quick_check(self)
-
-    def get_predicate(self):
+    def _get_predicate(self, model_variable):
         # returns a z3 predicate
-        self.model_variable = datatypes.new_model()
         predicate = self._assert(self.model_variable)
         return predicate
 
     def _assert(self, model):
         raise NotImplementedError("Implement _assert in subclasses.")
 
-    def check_implies(self, other_predicate):
-        raise NotImplementedError("TODO")
-
 
 def _ensure_predicate(thing):
     """Raise ValueError if thing is not an instance of Predicate."""
     if not isinstance(thing, Predicate):
         raise ValueError("Argument must be instance of Predicate. Instead, got %s" % repr(thing))
+
 
 class And(Predicate):
     def __init__(self, *preds):
